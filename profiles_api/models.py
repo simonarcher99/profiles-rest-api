5a5e9b29cf8bf958412948790687262485c2d26d
Simon Archer <simon.james.archer@gmail.com>
2021-10-13 15:11:10 +0100

    Added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
+
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
@@ -47,8 +49,21 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def get_short_name(self):
         """Retrieve short name of user"""
         return self.name
-    
+
     def __str__(self):
         """Return string representation of our user"""
         return self.email
 
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
